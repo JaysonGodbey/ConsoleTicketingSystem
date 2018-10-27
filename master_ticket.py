240fd6c5b012de5b560736e73d2382bbcd4e94bc
Jayson Godbey <43347663+JaysonGodbey@users.noreply.github.com>
2018-10-27 16:00:44 -0400

    Create master_ticket.py

---
+++ master_ticket.py
@@ -0,0 +1,33 @@
+SURCHARGE = 2
+TICKET_PRICE = 10
+
+tickets_remaining = 100
+
+# Create calc function takes number of tickets and retruns total
+def calculate_price(number_of_tickets):
+    
+    return (number_of_tickets * TICKET_PRICE) + SURCHARGE
+
+while tickets_remaining >= 1:
+    print("There are {} tickets remaining.".format(tickets_remaining))
+    name = input("What is your name?  ")
+    number_of_tickets = input("Hello, {}! How many tickets would you like to buy?  ".format(name))
+    try:
+        number_of_tickets = int(number_of_tickets)
+        if number_of_tickets > tickets_remaining:
+            raise ValueError("There are only {} tickets remaining.".format(tickets_remaining))
+    except ValueError as err:
+        print("Oh no, we ran into an issue. {} Please try again.".format(err))
+    else:
+        amount_due = calculate_price(number_of_tickets)
+    
+        print("Your total is ${}.".format(amount_due))
+        proceed = input("Would you like to proceed with the purchase Y/N?  ")
+        
+        if proceed.lower() == "y":
+            print("SOLD!")
+            tickets_remaining -= number_of_tickets
+        else:
+            print("Thank you anyways, {}.".format(name))
+
+print("Sorry, we are sold out of tickets.")
